894154c3ed3afcccfab5049ab200eb357725fe82
Developer9410 <developer9140@gmail.com>
2025-06-27 00:26:51 +0530

    Updated Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,11 +3,10 @@ import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 import Footer from './Footer';
-import BoltBadge from '../ui/BoltBadge';
 import { motion } from 'framer-motion';
 import { useApp } from '../../context/AppContext';
 
-const Layout = () => {
+const Layout: React.FC = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const { loading } = useApp();
   const location = useLocation();
@@ -47,9 +46,6 @@ const Layout = () => {
 
   return (
     <div className="min-h-screen flex flex-col bg-background">
-      <div className="relative">
-        <BoltBadge className="absolute top-4 left-4 sm:left-6 md:left-8 z-50" />
-      </div>
       <Navbar toggleSidebar={toggleSidebar} />
       <div className="flex flex-1">
         <Sidebar isOpen={sidebarOpen} closeSidebar={() => setSidebarOpen(false)} />
